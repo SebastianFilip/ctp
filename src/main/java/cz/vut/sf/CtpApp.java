da2810079eef532895fbd11e3bcc82900b507baf
sebastian <you@example.com>
2017-02-21 00:19:48 +0100

    first commit

---
+++ src/main/java/cz/vut/sf/CtpApp.java
@@ -0,0 +1,8 @@
+package cz.vut.sf;
+
+public class CtpApp {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cz/vut/sf/CtpAppTest.java
@@ -0,0 +1,8 @@
+package cz.vut.sf;
+import junit.framework.Assert;
+public class CtpAppTest {
+    public void testApp()
+    {
+        Assert.assertTrue( true );
+    }
+}
